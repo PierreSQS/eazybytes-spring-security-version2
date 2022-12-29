d49ae361b4c63bb8f53e0cb02032deb6a9f7be1d
PierreSQS <nomo_messina@web.de>
2022-12-30 00:45:32 +0100

    Sec3_ChapXX: pierrot optimizations and upgraded SB to 3.0.1

--- section3-pierrot/springsecsection3-pierrot/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ section3-pierrot/springsecsection3-pierrot/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -11,32 +11,12 @@ public class ProjectSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
-        /**
-         *  Below is the custom security configurations
-         */
-
         http.authorizeHttpRequests()
                         .requestMatchers("/myAccount","/myBalance","/myLoans","/myCards").authenticated()
                         .requestMatchers("/notices","/contact").permitAll()
                 .and().formLogin()
                 .and().httpBasic();
         return http.build();
-
-        /**
-         *  Configuration to deny all the requests
-         */
-        /*http.authorizeHttpRequests().anyRequest().denyAll()
-                .and().formLogin()
-                .and().httpBasic();
-        return http.build();*/
-
-        /**
-         *  Configuration to permit all the requests
-         */
-        /*http.authorizeHttpRequests().anyRequest().permitAll()
-                .and().formLogin()
-                .and().httpBasic();
-        return http.build();*/
     }
 
 }
