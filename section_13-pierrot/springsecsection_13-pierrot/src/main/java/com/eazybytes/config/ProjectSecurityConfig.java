6a4892258ea18ddfdb6a3534bf1c985e51773e79
PierreSQS <nomo_messina@web.de>
2023-02-02 10:00:41 +0100

    Sec13_Chap119-Setup EazyBytes Res. Server: set security configs like EazyBytes

--- section_13-pierrot/springsecsection_13-pierrot/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ section_13-pierrot/springsecsection_13-pierrot/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -1,12 +1,9 @@
 package com.eazybytes.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
@@ -50,11 +47,5 @@ public class ProjectSecurityConfig {
 
         return http.build();
     }
-
-    @Bean
-    public JwtDecoder jwtDecoder(@Value("${issuerUri}") String issuerUri) {
-        return JwtDecoders.fromIssuerLocation(issuerUri);
-    }
-
 
 }
